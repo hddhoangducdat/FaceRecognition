c08f54bb2aaddc077f4a6fbe6c2515ad3efe6fc3
hddhoangducdat <hddhoangducdat@gmail.com>
2019-03-26 06:46:56 +0700

    change file

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {
+  Component
+} from "react";
+import Navigation from "./Component/Navigation/Navigation"
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return ( 
+      <div className = "App" >
+      <Navigation/> 
+      {/* <Logo />
+          <ImageLinkForm/>
+          <FaceRecognition/>  */}
+      </div>
+    );
+  }
+}
+
+export default App;
